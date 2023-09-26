829a1a8226871a92f5983af0acdc919ba6f89dcb
stern14 <benjemintenison1@gmail.com>
2023-09-26 23:16:12 +0500

    Провел рефакторинг кода
    * Добавил функцию len_and_slug и задокументировал код в validators.py
    * Добавил комментарии к коду в robots/views.py

--- robots/validators.py
+++ robots/validators.py
@@ -1,31 +1,50 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 from django.core.validators import validate_slug
 from time import strptime
 
 
-def validate_even(data):
+def len_and_slug(fields: dict):
+    '''
+    Валидация данных модели и версии робота. 
+    Если данные валидны - возвращает True. Если нет - то причину ошибки.
+    Аргумент fields - это словарь, который содержит данные о модели и версии робота 
+    '''
+
+    for key, value in fields.items():
+        if len(value) != 2:
+            return f"Неккоректное наименование {key}! \
+Имя {key} должно быть длиной в 2(два) символа"
+
+        try:
+            validate_slug(value)
+        except ValidationError:
+            return f'Неккоректное наименование {key}! \
+Имя {key} состоит только из латинских букв, \
+цифр, символов подчеркивания или дефисов'
+
+    return True
+
+
+def validate_even(data: dict):
+    '''
+    Валидация входящих от пользователя данных о роботе.
+    Если данные валидны - возвращает True. Если нет - то причину ошибки.
+    Аргумент data - это словарь, который содержит входящие от пользователя данные о роботе
+    '''
+
     model = data['model']
     version = data['version']
     date = data['created']
+    fields = {'model': model, "version": version}
+    valid = len_and_slug(fields)
 
-    if len(model) != 2:
-        return f"Неккоректное наименование модели! Имя модели должно быть длиной в 2(два) символа"
-    try:
-        validate_slug(model)
-    except ValidationError:
-        return 'Неккоректное наименование модели! Имя модели состоит только из латинских букв, цифр, символов подчеркивания или дефисов'
-
-    if len(version) != 2:
-        return "Неккоректное наименование версии! Имя версии должно быть длиной в 2(два) символа"
-    try:
-        validate_slug(version)
-    except ValidationError:
-        return 'Неккоректное наименование модели! Имя модели состоит только из латинских букв, цифр, символов подчеркивания или дефисов'
-
-    try:
-        strptime(date, '%Y-%m-%d %H:%M:%S')
-    except ValueError:
-        return f'Не правильно введено время!\nПравильный формат времени: ГГ-ММ-ДД ЧЧ:ММ:СС\nПравильный пример: 2022-12-31 23:59:59\nИ что отправил ты: {date}'
-
-    return True
+    if valid == True:
+        try:
+            strptime(date, '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            return f'Не правильно введено время!\n\
+Правильный формат времени: ГГ-ММ-ДД ЧЧ:ММ:СС\n\
+Правильный пример: 2022-12-31 23:59:59\n\
+И что отправил ты: {date}'
+        return True
+    return valid
--- robots/views.py
+++ robots/views.py
@@ -9,18 +9,23 @@ from .validators import validate_even
 
 @csrf_exempt
 def api_robots(request):
+
     if request.method == "POST":
-        data = json.loads(request.body.decode())
+        #Загружаем и проверяем на валидность входящие данные
+        data: dict = json.loads(request.body.decode())
         validate = validate_even(data)
 
+        #Если данные корректны - сохраняем их в БД
         if validate == True:
-            robot = Robot(
+            robot: Robot = Robot(
                 serial=f"{data['model']}-{data['version']}",
                 model=data['model'],
                 version=data['version'],
                 created=data['created']
             )
             robot.save()
+            
             return HttpResponse('Запись сохраненна')
-
+        
+        #Если нет - отправляем причину ошибки
         return HttpResponse(validate)
