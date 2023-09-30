86e219d079bc3b1ba4e3b59bdcf9d0fcbce2d976
stern14 <benjemintenison1@gmail.com>
2023-10-01 01:11:02 +0500

    final commit

--- robots/signals.py
+++ robots/signals.py
@@ -9,6 +9,18 @@ from django.template.loader import render_to_string
 from .models import Robot
 
 
+def send_message(email, instance):
+    data = {
+        'model': instance.model,
+        'version': instance.version,
+    }
+
+    html_body = render_to_string('email_message.html', data)
+
+    msg = EmailMultiAlternatives(subject='Интересуемый робот в наличии', to = [email])
+    msg.attach_alternative(html_body, 'text/html')
+    msg.send()
+
 @receiver(post_save, sender=Robot)
 def check_waiting_list(instance, **kwargs):
     serial = instance.serial
@@ -25,20 +37,10 @@ def check_waiting_list(instance, **kwargs):
 
             if row > sheet.max_row:
                 break
-            
+
             if sheet[f'A{row}'].value == serial:
                 email_customer = sheet[f'B{row}'].value
-                data = {
-                    'model': instance.model,
-                    'version': instance.version,
-                }
-
-                html_body = render_to_string('email_message.html', data)
-
-                msg = EmailMultiAlternatives(subject='Интересуемый робот в наличии', to = [email_customer])
-                msg.attach_alternative(html_body, 'text/html')
-                msg.send()
-
+                send_message(email_customer, instance)
                 sheet.delete_rows(row)
 
             else:
